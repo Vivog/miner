bdf4185d489b334e659457701bfeeb61118e4cef
vivgg <vivog2017@gmail.com>
2022-07-07 11:44:45 +0300

    2

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ class GamePole:
         self.N = N
         self.M = M
         self.pole = []
-        self.init()
         self.var = range(0, self.N * self.N)
+        self.init()
 
     def mines_pos(self):
         mines_pos = []
@@ -22,36 +22,63 @@ class GamePole:
                 mines_pos.append(mine)
             else:
                 continue
-        print(mines_pos)
         return mines_pos
 
     def around_mines(self):
         pos = 0
         mines_pos = self.mines_pos()
+        print(mines_pos)
         pole = []
         for i in self.var:
             if pos in mines_pos:
                 pole.append(Cell(0, True))
             else:
                 pole.append(Cell(0, False))
+            pos += 1
+        return pole
+
 
 
     def init(self):
-        pos = 0
-        mines_pos = self.mines_pos()
-        for cell in range(0, self.N):
-            self.pole.append([])
-            for elem in range(0, self.N):
-                if len(self.pole[cell]) < self.N:
-                    if pos in mines_pos:
-                        self.pole[cell].append(Cell(0, True))
-                    else:
-                        self.pole[cell].append(Cell(0, False))
-                    pos += 1
-        print(self.pole)
-
-
-
+        pole = self.around_mines()
+        while len(pole):
+            self.pole.append(list(pole[:self.N]))
+            del pole[:self.N]
+        for i in range(len(self.pole)):
+            for j in range(len(self.pole[i])):
+                if self.pole[i][j].mine == True:
+                    try:
+                        self.pole[i][j - 1].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i][j + 1].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i-1][j - 1].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i-1][j + 1].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i-1][j].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i+1][j - 1].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i+1][j + 1].around_mines += 1
+                    except:
+                        continue
+                    try:
+                        self.pole[i+1][j].around_mines += 1
+                    except:
+                        continue
 
     def show(self):
         for i in self.pole:
@@ -59,7 +86,7 @@ class GamePole:
                 if j.mine == True:
                     print('m', end=' ')
                 else:
-                    print('0', end=' ')
+                    print(j.around_mines, end=' ')
             print()
 
 
