e9c816d8f8970ccc00cc4edea928c92d7833c7e8
VIVOG <vivog2017@gmail.com>
2022-07-07 20:20:40 +0300

    1

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ class Cell:
     def __init__(self, around_mines, mine: bool):
         self.around_mines = around_mines
         self.mine = mine
-        self.fl_open = False
+        self.fl_open = True
 
 
 class GamePole:
@@ -27,7 +27,7 @@ class GamePole:
     def around_mines(self):
         pos = 0
         mines_pos = self.mines_pos()
-        print(mines_pos)
+        # print(mines_pos)
         pole = []
         for i in self.var:
             if pos in mines_pos:
@@ -37,56 +37,82 @@ class GamePole:
             pos += 1
         return pole
 
-
-
-    def init(self):
+    def s_pole(self):
         pole = self.around_mines()
         while len(pole):
             self.pole.append(list(pole[:self.N]))
             del pole[:self.N]
+        return self.pole
+
+
+    def init(self):
+        self.pole = self.s_pole()
         for i in range(len(self.pole)):
             for j in range(len(self.pole[i])):
                 if self.pole[i][j].mine == True:
-                    try:
-                        self.pole[i][j - 1].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i][j + 1].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i-1][j - 1].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i-1][j + 1].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i-1][j].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i+1][j - 1].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i+1][j + 1].around_mines += 1
-                    except:
-                        continue
-                    try:
-                        self.pole[i+1][j].around_mines += 1
-                    except:
-                        continue
+                    if i == 0:
+                        if j == 0:
+                            self.pole[i][j + 1].around_mines += 1
+                            self.pole[i + 1][j].around_mines += 1
+                            self.pole[i + 1][j + 1].around_mines += 1
+                        if j == len(self.pole[i]) - 1:
+                            self.pole[i][j - 1].around_mines += 1
+                            self.pole[i + 1][j].around_mines += 1
+                            self.pole[i + 1][j - 1].around_mines += 1
+                        if (len(self.pole[i]) - 1) > j > 0:
+                            self.pole[i][j + 1].around_mines += 1
+                            self.pole[i][j - 1].around_mines += 1
+                            self.pole[i + 1][j].around_mines += 1
+                            self.pole[i + 1][j + 1].around_mines += 1
+                            self.pole[i + 1][j - 1].around_mines += 1
+                    if i == len(self.pole) - 1:
+                        if j == 0:
+                            self.pole[i][j + 1].around_mines += 1
+                            self.pole[i - 1][j].around_mines += 1
+                            self.pole[i - 1][j + 1].around_mines += 1
+                        if j == len(self.pole[i]) - 1:
+                            self.pole[i][j - 1].around_mines += 1
+                            self.pole[i - 1][j].around_mines += 1
+                            self.pole[i - 1][j - 1].around_mines += 1
+                        if(len(self.pole[i]) - 1) > j > 0:
+                            self.pole[i][j + 1].around_mines += 1
+                            self.pole[i][j - 1].around_mines += 1
+                            self.pole[i - 1][j].around_mines += 1
+                            self.pole[i - 1][j + 1].around_mines += 1
+                            self.pole[i - 1][j - 1].around_mines += 1
+                    if (len(self.pole) - 1) > i > 0:
+                        if j == 0:
+                            self.pole[i][j + 1].around_mines += 1
+                            self.pole[i - 1][j].around_mines += 1
+                            self.pole[i - 1][j + 1].around_mines += 1
+                            self.pole[i + 1][j].around_mines += 1
+                            self.pole[i + 1][j + 1].around_mines += 1
+                        if j == len(self.pole[i]) - 1:
+                            self.pole[i][j - 1].around_mines += 1
+                            self.pole[i - 1][j].around_mines += 1
+                            self.pole[i - 1][j - 1].around_mines += 1
+                            self.pole[i + 1][j].around_mines += 1
+                            self.pole[i + 1][j - 1].around_mines += 1
+                        if (len(self.pole[i]) - 1) > j > 0:
+                            self.pole[i][j + 1].around_mines += 1
+                            self.pole[i][j - 1].around_mines += 1
+                            self.pole[i - 1][j].around_mines += 1
+                            self.pole[i - 1][j + 1].around_mines += 1
+                            self.pole[i - 1][j - 1].around_mines += 1
+                            self.pole[i + 1][j].around_mines += 1
+                            self.pole[i + 1][j + 1].around_mines += 1
+                            self.pole[i + 1][j - 1].around_mines += 1
 
     def show(self):
         for i in self.pole:
             for j in i:
-                if j.mine == True:
-                    print('m', end=' ')
+                if j.fl_open == False:
+                    print('#', end=' ')
                 else:
-                    print(j.around_mines, end=' ')
+                    if j.mine == True:
+                        print('*', end=' ')
+                    else:
+                        print(j.around_mines, end=' ')
             print()
 
 
